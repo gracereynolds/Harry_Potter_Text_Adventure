dcd79e15a1f6cce4683d783463f303eda195bdf5
gkr <greynolds7248@bths.edu>
2018-12-03 08:41:20 -0500

    started player

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,4 +1,15 @@
 package com.company;
 
 public class Player {
+    public String name, House;
+    public int x, y;
+    int points;
+    public Player(String name, String House)
+    {
+        this.name = name;
+        this.House = House;
+        this.x = 0;
+        this.y = 0;
+        this.points = 0;
+    }
 }
