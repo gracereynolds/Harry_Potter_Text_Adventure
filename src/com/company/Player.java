3be1b91a9995ed83136c5b766e105fdbfd82a092
gkr <greynolds7248@bths.edu>
2018-12-04 08:19:35 -0500

    person class done pending discovering we need to add something else

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -12,4 +12,20 @@ public class Player {
         this.y = 0;
         this.points = 0;
     }
+    public int getX()
+    {
+        return x;
+    }
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+    public int getY()
+    {
+        return y;
+    }
+    public void setY(int y)
+    {
+        this.y = y;
+    }
 }
