54e80ac2a6756b8e8959e71a3366af7411cec8d5
BT_1N3_13 <a>
2018-12-06 08:41:36 -0500

    Updated main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        System.out.println("Welcome to a Harry Potter based Text Adventure");
 	// write your code here
     }
 }
