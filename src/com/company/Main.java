2531d10fc890597602b23bd5a76a189a2629b870
jleung1239 <jleung1239@bths.edu>
2018-12-13 00:55:09 -0500

    updated main class (but it needs lots of fixing)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,46 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static boolean gameOn = true;
+
+
     public static void main(String[] args) {
 
+        // fill board with empty hallways
+        Room[][] board = new Room[5][5];
+        String[][] boardprint = new String[5][5];
+        for (int x = 0; x < board.length; x++){
+            for (int y = 0; y < board[x].length; y++){
+                board[x][y] = new Room(x,y);
+                boardprint[x][y] = "[o]";
+            }
+        }
+        
+
+
+        // create random winning room thats not at 0,0
+        int x = (int)(Math.random()*board.length);
+        int y = (int)(Math.random()*board.length);
+        while (x + y == 0){
+            x = (int)(Math.random()*board.length);
+            y = (int)(Math.random()*board.length);
+        }
+        board[x][y] = new ClassRoom(x,y);
+        int XClassroom = x;
+        int YClassroom = y;
+
+        // this is so that any Filch won't overwrite the Classroom
+        while ((x == XClassroom) && (y == YClassroom)){
+            x = (int)(Math.random()*board.length);
+            y = (int)(Math.random()*board.length);
+        }
+
+        board[x][y] = new Filch(x,y);
+        //add the rest of the rooms into the board
+
+
         Scanner in = new Scanner (System.in);
-        System.out.println("Welcome to a Harry Potter based Text Adventure.");
+        System.out.println("Welcome to a Harry Potter themed Text Adventure.");
         System.out.println("You are a student attending Hogwarts, the school of Witchcraft and Wizardry.");
         System.out.println("What's your name, student?");
         String name = in.nextLine();
@@ -32,7 +68,86 @@ public class Main {
 
         String house = statement.substring(0,1).toUpperCase() + statement.substring(1);
         System.out.println("Welcome to " +house+ ", " +name+ "!");
+        System.out.println("");
+        //unfinished: describe setting and rules and stuff, introductory
 
-	// write your code here
+        Player player1 = new Player(name,house);
+        board[0][0].enterRoom(player1);
+        while (gameOn){
+            System.out.println(boardprint);
+            System.out.println("Where would you like to move? (Choose N, S, E, W)");
+            String move = in.nextLine();
+            if(validMove(move, player1, board))
+            {
+                System.out.println(boardprint);
+            }
+            else {
+                System.out.println("Please choose a valid move.");
+            }
+        }
+        in.close();
+
     }
+
+    public static boolean validMove(String move, Player a, Room[][] map)
+    {
+        move = move.toLowerCase().trim();
+        switch (move) {
+            case "n":
+                if (a.getX() > 0)
+                {
+                    map[a.getX()][a.getY()].exitRoom(a);
+                    map[a.getX()-1][a.getY()].enterRoom(a);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            case "e":
+                if (a.getY()< map[a.getY()].length -1)
+                {
+                    map[a.getX()][a.getY()].exitRoom(a);
+                    map[a.getX()][a.getY() + 1].enterRoom(a);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            case "s":
+                if (a.getX() < map.length - 1)
+                {
+                    map[a.getX()][a.getY()].exitRoom(a);
+                    map[a.getX()+1][a.getY()].enterRoom(a);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            case "w":
+                if (a.getY() > 0)
+                {
+                    map[a.getX()][a.getY()].exitRoom(a);
+                    map[a.getX()][a.getY()-1].enterRoom(a);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            default:
+                break;
+
+        }
+        return true;
+    }
+    public static void gameOff()
+    {
+        gameOn = false;
+    }
+
 }
